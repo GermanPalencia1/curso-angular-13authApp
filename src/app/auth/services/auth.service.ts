68e2eb211adf32a551f4ea12968e0d170fa8e5a0
Germán <german.palencia@adentia.es>
2023-10-06 12:48:10 +0200

    AuthService structure

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { enviroment } from 'src/enviroments/eviroments';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
+  private readonly baseUrl: string = enviroment.baseUrl;
+  private http = inject(HttpClient);
+
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>();
+
   constructor() { }
+
+  login(email: string, passwor: string): Observable<boolean> {
+
+    return of (true);
+
+  }
+
 }
